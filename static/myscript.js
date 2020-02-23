f689281d7b0b0e2a239871e813a44a548ad6edfd
Dev-Akash <sriakash.dev@gmail.com>
2020-02-23 06:36:25 +0530

    Initial Work

---
+++ static/myscript.js
@@ -0,0 +1,7 @@
+function open_navigation_menu() {
+    document.getElementById("navbar").style.display = "block";
+}
+
+function close_navigation_menu(params) {
+    document.getElementById("navbar").style.display = "none";
+}
