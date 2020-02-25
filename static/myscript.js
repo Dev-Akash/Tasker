4a0c80348bfe83300adca4e5ace278cdcf964df6
Dev-Akash <sriakash.dev@gmail.com>
2020-02-25 09:13:38 +0530

    Fixed Sign-up button issue

--- static/myscript.js
+++ static/myscript.js
@@ -7,15 +7,19 @@ function close_navigation_menu(params) {
 }
 
 function checkPass(){
-    password = document.getElementById("enter_pass").value
-    re_password = document.getElementById("re_enter_pass").value
+    password = document.getElementById("enter_pass").value;
+    re_password = document.getElementById("re_enter_pass").value;
     if (re_password != password){
-        document.getElementById("enter_pass").style.border = "2px solid red"
-        document.getElementById("re_enter_pass").style.border = "2px solid red"
+        document.getElementById("enter_pass").style.border = "2px solid red";
+        document.getElementById("re_enter_pass").style.border = "2px solid red";
+        document.getElementById("sign_up_button").disabled = true;
+        document.getElementById("sign_up_button").style.opacity = 0.5;
     }
     else{
-        document.getElementById("enter_pass").style.border = "none"
-        document.getElementById("re_enter_pass").style.border = "none"
+        document.getElementById("enter_pass").style.border = "none";
+        document.getElementById("re_enter_pass").style.border = "none";
+        document.getElementById("sign_up_button").disabled = false;
+        document.getElementById("sign_up_button").style.opacity = 1;
     }
 }
 
